2d577f78f476c145d4a511b782cb075f1f1e863a
unknown <farizbaiquni@gmail.com>
2021-10-25 03:50:01 +0700

    initial commit

---
+++ app/src/main/java/com/example/classcollaboration/CraateAccountSchool2.kt
@@ -0,0 +1,11 @@
+package com.example.classcollaboration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CraateAccountSchool2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_craate_account_school2)
+    }
+}
---
+++ app/src/main/java/com/example/classcollaboration/CreateAccountSchool1.kt
@@ -0,0 +1,11 @@
+package com.example.classcollaboration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountSchool1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account_school1)
+    }
+}
